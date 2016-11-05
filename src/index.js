715cde59ce764a9dc78182e438dbcd9311f54712
Cristhian <valenciacristhian47@gmail.com>
2016-11-05 12:34:37 +0000

    Iniciando medium-clone

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const defaultState = {
+  appName: 'medium-clone',
+  articles: null
+};
+const reducer = function(state = defaultState, action) {
+  return state;
+};
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
